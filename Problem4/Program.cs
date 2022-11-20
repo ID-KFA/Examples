66db09293a8a06b39c180425cf8b272d23e6583b
Наш компьютер <antonnovoselov@iMac.local>
2022-11-20 22:33:18 +0500

    Added Problems 2,4,6,8

---
+++ Problem4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+int max=0;
+if (x>y)
+{
+   max=x;
+}
+
+else 
+{
+ max=y;
+}
+
+if (max<z)
+{
+ max = z; 
+}
+
+
+ Console.WriteLine($"Максимальное число {max} найдено"); 
